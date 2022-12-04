a690c713423678664896190519782e198fe0d1aa
David Ferguson <dvf@gmx.com>
2022-12-04 21:30:17 +0000

    Outline of project

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+// Rock Paper Scissors Game.
+
+// Full game has 5 rounds. Loop 'round' 5 times
+
+// Each round choices are compared and a winner or tie is announced
+
+// Computer chooses from random number * 100. Range bias possible
+
+// Player chooses from the 3 options
+
