767567646eba7d7e898d09b5f227e00e2336c48a
David Ferguson <dvf@gmx.com>
2023-02-11 20:46:59 +0000

    Add functions for player and game, finish round function

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 
 
 // Rock Paper Scissors Game.
+let choices = ["rock", "paper", "scissors"]
 
 // Full game has 5 rounds. Loop 'round' 5 times
 
 // Each round choices are compared and a winner or tie is announced
 
-// Computer chooses from random number * 100. Range bias possible
-
 // Player chooses from the 3 options
 
+// function for computer choice
+function getComputerChoice(choices){
+    let index = Math.floor(Math.random() * choices.length)
+    return choices[index]
+}
