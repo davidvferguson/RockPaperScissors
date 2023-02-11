4734f741f58d5b852b639e542b7cce48800c96a0
David Ferguson <dvf@gmx.com>
2023-02-11 23:09:08 +0000

    Fix various errors to get working

--- script.js
+++ script.js
@@ -2,17 +2,20 @@
 
 // Rock Paper Scissors Game.
 
-let playerScore = 0
-let computerScore = 0
+
+
+let playerScore = 0;
+let computerScore = 0;
+game();
 
 //console.log(playRound(playerSelection, computerSelection));
 
 //play 5 rounds
 function game(){
-    console.log("game function started")
+   // console.log("game function started")
     for (let i=0; i < 5; i++){
         playRound();
-        console.log(roundResult);
+      //  console.log(playRound());
     }
     if (playerScore > computerScore){
         console.log("You Win! " + playerScore + " - " + computerScore)
@@ -23,43 +26,55 @@ function game(){
     else console.log("Game Tied! " + playerScore + " - " + computerScore)
 }
 
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
-
 // 1 round of game
-function playRound(playerSelection, computerSelection){
-    console.log("playRound function started")
+function playRound(playerChoice, computerChoice)
+
+ {
+   // console.log("playRound function started");
     let roundResult = "";
-    if (playerSelection === computerSelection){
-    roundResult = "Tie!";
-    }
-    else if ((playerSelection == "rock" && computerSelection == "scissors")
-    || (playerSelection == "scissors" && computerSelection == "paper")
-    || (playerSelection == "paper" && computerSelection == "rock")){
-    roundResult = "You win this round, " + playerSelection + "beats " + computerSelection;
-    playerScore++;
-    }
-    else {
-        roundResult = "You lose this round!" + playerSelection + "is beaten by " + computerSelection;
-    computerScore++;
-    }
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+
+    if (playerSelection === computerSelection)
+        {
+        roundResult = "Round is a Tie!";
+        }
+
+    else if ((playerSelection === "rock" && computerSelection === "scissors")||
+        (playerSelection === "scissors" && computerSelection === "paper")||
+        (playerSelection === "paper" && computerSelection === "rock"))
+        {
+        roundResult = "You win this round, " + playerSelection + " beats " + computerSelection;
+        playerScore++;
+        }
+
+    else 
+        {
+        roundResult = "You lose this round! " + playerSelection + " is beaten by " + computerSelection;
+        computerScore++;
+        }
+
+    console.log(roundResult)
+    console.log(playerScore + " - " + computerScore)
     return roundResult;
 }
 
 // function for computer choice
 function getComputerChoice(){
-    console.log("getComputerChoice function started")
-    const computerChoice = ["rock", "paper", "scissors"];
-    let choice = Math.floor(Math.random() * computerChoice.length);
-    return computerChoice[choice];
-    console.log("Computer picked " + computerChoice);
+    //console.log("getComputerChoice function started")
+    const choice = ["rock", "paper", "scissors"];
+    let computerChoice = Math.floor(Math.random() * choice.length);
+    console.log("Computer picked " + choice[computerChoice]);
+    return choice[computerChoice];
+    
 }
 
 // function for player choice
 function getPlayerChoice(){
-    console.log("getPlayerChoice function started")
+    //console.log("getPlayerChoice function started")
     let input = prompt("Enter 'Rock', 'Paper', or 'Scissors'");
     let playerChoice = input.toLowerCase();
-    return playerChoice;
     console.log("You picked " + playerChoice);
+    return playerChoice;
+    
 }
